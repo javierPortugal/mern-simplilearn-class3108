c03733bf9803462c09812e7b1dabe2440b76e5ea
Javier Portugal <javier.s.portugal@gmail.com>
2024-09-01 08:09:16 -0600

    Create MultidimensionalArrays.js
    se agrega archivo js multidemensionall arrays

---
+++ javascript/MultidimensionalArrays.js
@@ -0,0 +1,10 @@
+//two dimensional arrays
+
+let myNumbers = [[1,2,3,4,5,],[6,7,8,9,10],[11,12,113,14,15]];
+console.log(myNumbers.length);
+console.log(myNumbers);
+
+for (let i=0; i<myNumbers.length; i++){
+    console.log("Array at index : ", i, " is ", myNumbers[i]);
+    
+}
